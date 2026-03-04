84dd6a2e5bc259ab8698e7199e39d6c8e411b809
Adrian Geada <adrigeada@gmail.com>
2026-03-04 21:18:31 +0100

    pilas

--- programacion_ud7/src/main/java/org/example/Ejercicios.java
+++ programacion_ud7/src/main/java/org/example/Ejercicios.java
@@ -7,36 +7,70 @@ public class Ejercicios {
     static void main() {
         Scanner teclado = new Scanner(System.in);
 
-        Stack<Character> pilaLetras = new Stack<>();
+//        Stack<Character> pilaLetras = new Stack<>();
+//
+//        pilaLetras.add('a');
+//        pilaLetras.add('b');
+//        pilaLetras.add('c');
+//        pilaLetras.add('d');
+//        pilaLetras.add('e');
+//
+//        System.out.println("La pila está vacía? : "+pilaLetras.isEmpty());
+//        System.out.println("Hay "+pilaLetras.size()+" caracteres.");
+//        System.out.println("El último elemento es: "+pilaLetras.peek());
+//
+//        do {
+//            System.out.println(pilaLetras.pop());
+//        }while (!pilaLetras.isEmpty());
+//
+//        Stack<Integer> pilaNum = new Stack<>();
+//
+//        System.out.print("Introduce el primer número:");
+//        pilaNum.push(teclado.nextInt());
+//        System.out.print("Segundo:");
+//        pilaNum.push(teclado.nextInt());
+//        System.out.print("Tercero:");
+//        pilaNum.push(teclado.nextInt());
+//        System.out.println("\nNúmeros en orden inverso:");
+//
+//        do {
+//            System.out.println(pilaNum.pop());
+//        }while (!pilaNum.isEmpty());
 
-        pilaLetras.add('a');
-        pilaLetras.add('b');
-        pilaLetras.add('c');
-        pilaLetras.add('d');
-        pilaLetras.add('e');
+        if (esBalanceada(")")){
+            System.out.println("BIen");
+        }else {
+            System.out.println("Mal");
+        }
 
-        System.out.println("La pila está vacía? : "+pilaLetras.isEmpty());
-        System.out.println("Hay "+pilaLetras.size()+" caracteres.");
-        System.out.println("El último elemento es: "+pilaLetras.peek());
 
-        do {
-            System.out.println(pilaLetras.pop());
-        }while (!pilaLetras.isEmpty());
 
-        Stack<Integer> pilaNum = new Stack<>();
+    }
 
-        System.out.print("Introduce el primer número:");
-        pilaNum.push(teclado.nextInt());
-        System.out.print("Segundo:");
-        pilaNum.push(teclado.nextInt());
-        System.out.print("Tercero:");
-        pilaNum.push(teclado.nextInt());
-        System.out.println("\nNúmeros en orden inverso:");
+    public static boolean esBalanceada(String cadena){
 
-        do {
-            System.out.println(pilaNum.pop());
-        }while (!pilaNum.isEmpty());
+        Stack<Character> pilaChar = new Stack<>();
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == '(') {
+                pilaChar.push(cadena.charAt(i));
 
+
+            }else if (cadena.charAt(i) == ')'){
+
+                if (pilaChar.contains('(')){
+                    pilaChar.pop();
+                }else {
+                    return false;
+                }
+            }
+
+        }
+
+        if (pilaChar.isEmpty()){
+            return true;
+        }else {
+            return false;
+        }
 
 
     }
---
+++ programacion_ud7/src/main/java/org/example/reto141.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class reto141 {
+
+    static void main(String[] args) {
+        while (esBalanceada()){
+
+        }
+    }
+
+
+    public static boolean esBalanceada(){
+        Scanner teclado = new Scanner(System.in);
+        Character[] caracteresAbiertos = {'(','[','{'};
+        Character[] caracteresCerrados = {')',']','}'};
+            if (!teclado.hasNext()){
+                return false;
+            }else {
+                String expresion = teclado.nextLine();
+                Stack<Character> pila = new Stack<>();
+
+                for (int i = 0; i < expresion.length(); i++) {
+                    int indiceAbierto = 4;
+                    int indiceCerrado = 5;
+
+                    if (Arrays.asList(caracteresAbiertos).contains(expresion.charAt(i))){
+                        pila.push(expresion.charAt(i));
+                    }
+
+                    if (Arrays.asList(caracteresCerrados).contains(expresion.charAt(i))){
+
+                        if (pila.isEmpty()){
+                            System.out.println("NO");
+                            return true;
+                        }else {
+                            indiceAbierto = Arrays.asList(caracteresAbiertos).indexOf(pila.peek());
+                            indiceCerrado = Arrays.asList(caracteresCerrados).indexOf(expresion.charAt(i));
+
+                            if (indiceAbierto == indiceCerrado){
+                                pila.pop();
+                            }
+
+                        }
+
+                    }
+
+
+                }
+
+                if (pila.isEmpty()){
+                    System.out.println("YES");
+                }else {
+                    System.out.println("NO");
+                }
+                return true;
+            }
+
+
+
+    }
+}
