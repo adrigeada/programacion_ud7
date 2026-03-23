949f11ed574394099cef25fe1ae5f6f3d6595734
Adrian Geada <adrigeada@gmail.com>
2026-03-23 18:04:02 +0100

    PreExamen

---
+++ programacion_ud7/src/main/java/org/example/preExamen/ChatBot.java
@@ -0,0 +1,44 @@
+package org.example.preExamen;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ChatBot {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+        String pregunta = "";
+
+        HashMap<String,String> mapaChat = new HashMap<>();
+
+        mapaChat.put("hola","Hola,en que te puedo ayudar.");
+        mapaChat.put("como estas","Estoy listo para ayudarte");
+        mapaChat.put("adios","Hasta luego");
+        mapaChat.put("gracias","De nada, que tengas buen dia.");
+
+        System.out.println("Bienvenido al chatbot");
+
+        while (true){
+
+            System.out.print("Tú:");
+            pregunta= teclado.nextLine().toLowerCase();
+
+            if (pregunta.equalsIgnoreCase("salir")){
+                System.out.println("Chatbot: Adiós");
+                break;
+            }
+
+            if (mapaChat.containsKey(pregunta)){
+
+                System.out.println("Chatbot: "+mapaChat.get(pregunta));
+
+            }else {
+                System.out.println("Chatbot: no te entiendo :(");
+            }
+
+        }
+
+
+
+
+    }
+}
